d65572c5503783472b0a55d6bb484d5dc01686bb
Haritha Hasathcharu <42366642+hasathcharu@users.noreply.github.com>
2022-12-24 14:48:25 +0530

    start screen draft complete

--- Freshlyy/components/Texts.js
+++ Freshlyy/components/Texts.js
@@ -1,17 +1,59 @@
 import React from 'react';
-import { Text, TurboModuleRegistry } from 'react-native';
+import { Text } from 'react-native';
 
-module.exports.P = function P(props) {
+module.exports.P = function (props) {
   return (
-    <Text style={{ fontFamily: 'Poppins', fontWeight: 'Light' }}>
+    <Text
+      style={{
+        fontFamily: 'Poppins',
+        fontWeight: 'Light',
+        ...props.style,
+      }}
+    >
       {props.children}
     </Text>
   );
 };
 
-module.exports.H1 = function H1(props) {
+module.exports.H1 = function (props) {
   return (
-    <Text style={{ fontFamily: 'Poppins', fontSize: 30, fontWeight: 'Bold' }}>
+    <Text
+      style={{
+        fontFamily: 'Poppins',
+        fontSize: 50,
+        fontWeight: 'Bold',
+        ...props.style,
+      }}
+    >
+      {props.children}
+    </Text>
+  );
+};
+module.exports.H2 = function (props) {
+  return (
+    <Text
+      style={{
+        fontFamily: 'Poppins',
+        fontSize: 30,
+        fontWeight: 'Bold',
+        ...props.style,
+      }}
+    >
+      {props.children}
+    </Text>
+  );
+};
+
+module.exports.H3 = function (props) {
+  return (
+    <Text
+      style={{
+        fontFamily: 'Poppins',
+        fontSize: 20,
+        fontWeight: 'light',
+        ...props.style,
+      }}
+    >
       {props.children}
     </Text>
   );
---
+++ Freshlyy/screens/StartScreen.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
+import { H2, H3 } from '../components/Texts';
+import { BigButton } from '../components/Buttons';
+
+import Theme from '../constants/theme';
+
+export default function () {
+  return (
+    <View style={styles.screen}>
+      <ImageBackground
+        source={require('../assets/start3.jpg')}
+        style={styles.bgImage}
+        resizeMode='cover'
+      >
+        <View style={styles.content}>
+          <View style={styles.logoContainer}>
+            <Image
+              source={require('../assets/logowhite.png')}
+              style={styles.logo}
+            />
+          </View>
+          <View style={styles.descContainer}>
+            <H2 style={styles.text}>Welcome</H2>
+            <H3 style={{ ...styles.text, ...styles.description }}>
+              Fresh Produce. To the Doorstep.
+            </H3>
+            <BigButton title='Get Started' />
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+  bgImage: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#00000044',
+  },
+  logoContainer: {
+    width: '80%',
+    height: 100,
+  },
+  text: {
+    color: Theme.contrastTextColor,
+    textAlign: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  description: {
+    marginBottom: 40,
+  },
+  logo: {
+    resizeMode: 'contain',
+    width: '100%',
+    height: '100%',
+    shadowColor: 'white',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+});
