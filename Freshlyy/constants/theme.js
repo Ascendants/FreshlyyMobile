0b6b91b6ccdf3b4fb650722f64d37f0f68f2d275
Komuthu <komuthuapsara@gmail.com>
2022-12-28 00:10:35 +0530

    infor card DB

--- Freshlyy/constants/theme.js
+++ Freshlyy/constants/theme.js
@@ -1,6 +1,7 @@
 export default {
   primary: '#10AB68',
   primaryShade: '#10AB6840',
+  primaryShadeLighter: '#10AB6820',
   textColor: '#555555',
   contrastTextColor: 'white',
   overlay: '#e0e0e0',
--- Freshlyy/screens/DashBoard.js
+++ Freshlyy/screens/DashBoard.js
@@ -1,22 +1,28 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Image, Text } from "react-native";
 import { H1, P } from '../components/Texts';
+import Header from "../components/Header";
 import Navbar from "../components/Navbar";
+import InfoCardDB from "../components/InfoCardDB";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function (){
     return(
-        <View style={styles.screen}>
-            <H1>Hello</H1>
-            <Navbar />
-        </View>
+        <SafeAreaView>
+            <View style={styles.screen}>
+                <Header />
+                <InfoCardDB />
+                {/* <Navbar /> */}
+            </View>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     screen: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      fontFamily: 'Poppins',
-    },
+        // flex:1,
+        // justifyContent: 'center',
+        // alignContent: 'center',
+        fontFamily: 'Poppins',
+    }
   });
