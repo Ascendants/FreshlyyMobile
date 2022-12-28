a84328f4c7dece353292130a821a5ead45fa85a8
Komuthu <komuthuapsara@gmail.com>
2022-12-28 12:38:23 +0530

    navbar position

--- Freshlyy/components/Header.js
+++ Freshlyy/components/Header.js
@@ -3,16 +3,20 @@ import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import Theme from '../constants/theme';
 
-export default function () {
+export default function (props) {
   return (
     <View style={styles.container}>
+      {props.back?
       <TouchableOpacity>
         <Ionicons name='ios-chevron-back' size={32} color={Theme.textColor} />
       </TouchableOpacity>
+      :<View></View>}
       <Image source={require('../assets/logo.png')} style={styles.logo} />
+      {props.back?
       <TouchableOpacity>
         <Ionicons name='ios-menu' size={32} color={Theme.textColor} />
       </TouchableOpacity>
+      :<View></View>}
     </View>
   );
 }
