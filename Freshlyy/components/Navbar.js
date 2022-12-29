d0c6f81435e9c8e31cc5a094fad963c5b9b4bcfb
Komuthu <komuthuapsara@gmail.com>
2022-12-29 18:01:38 +0530

    Dashboard static view complete

--- Freshlyy/components/Navbar.js
+++ Freshlyy/components/Navbar.js
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'flex-end',
         width:'100%',
-        bottom: 20,
+        bottom: 0,
             
     },
     navbar: {
