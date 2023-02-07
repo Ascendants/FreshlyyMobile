c9a4ab9629985f6a9af7d88f832e8918820d17f2
Haritha Hasathcharu <42366642+hasathcharu@users.noreply.github.com>
2023-02-07 13:42:32 +0530

    firebase config change

--- Freshlyy/utils/firebase.js
+++ Freshlyy/utils/firebase.js
@@ -1,12 +1,11 @@
 import { initializeApp } from 'firebase/app';
-import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
-  apiKey: 'AIzaSyCx4oohXi6uKe84dQsPjksqa3kWa6JEOK4',
-  authDomain: 'freshlyyimagestore.firebaseapp.com',
-  projectId: 'freshlyyimagestore',
-  storageBucket: 'freshlyyimagestore.appspot.com',
-  messagingSenderId: '575461909176',
-  appId: '1:575461909176:web:27df3be08664ab63224a4d',
+  apiKey: 'AIzaSyBiMF12vtgUGyUlxgY-sq5Rqn5ama1WFR4',
+  authDomain: 'freshlyy-437ac.firebaseapp.com',
+  projectId: 'freshlyy-437ac',
+  storageBucket: 'freshlyy-437ac.appspot.com',
+  messagingSenderId: '940915596506',
+  appId: '1:940915596506:web:84b0cf3b8f5d962e2e19e3',
 };
 
 const app = initializeApp(firebaseConfig);
