7b14250a9785b3f30a80af42b822d28abb781b1a
Haritha Hasathcharu <42366642+hasathcharu@users.noreply.github.com>
2023-02-16 10:38:24 +0530

    small fixes

--- Freshlyy/components/ProductCard.js
+++ Freshlyy/components/ProductCard.js
@@ -1,7 +1,7 @@
 import { contains } from '@firebase/util';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import { H1, H2,H3,H5,H7,H6,Pr,P} from '../components/Texts';
+import { H1, H2, H3, H5, H7, H6, Pr, P } from '../components/Texts';
 import Theme from '../constants/theme';
 import { FilledBigButton } from '../components/Buttons';
 import theme from '../constants/theme';
@@ -9,50 +9,65 @@ import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 import { AntDesign } from '@expo/vector-icons';
 import Rating from '../components/Rating';
 
-
 export default function (props) {
-  const [like,setLike]=useState(false)
-  const [likecount,setLikeCount]=useState(props.like)
-  
-   function handleLike(){
-    setLike(prevLike=>!prevLike)
-    return like
+  const [like, setLike] = useState(false);
+  const [likecount, setLikeCount] = useState(props.like);
+
+  function handleLike() {
+    setLike((prevLike) => !prevLike);
+    return like;
   }
 
-
-
-
- 
-
   return (
-      <TouchableOpacity>
-      <View style={[styles.card,props.cardType=='social'?{height:250}:{height:255}]} >
+    <TouchableOpacity>
+      <View
+        style={[
+          styles.card,
+          props.cardType == 'social' ? { height: 250 } : { height: 255 },
+        ]}
+      >
         <View style={styles.imgcont}>
-        <Image
-          source={{ uri: props.imageUrl }}
-          style={styles.cardimage}
-        />
+          <Image source={{ uri: props.imageUrl }} style={styles.cardimage} />
         </View>
-       
+
         <View style={styles.desccont}>
-        <H7 style={styles.prodname}>{props.title}</H7>
-        <View style={styles.reviewCont}> 
-          <Rating value={props.overallRating} />
-          <P>({props.overallRating})</P>
-        </View>
-      
-        <Pr>{props.price}/{props.unit}</Pr>
-        <H6>By Haritha</H6>
-        {props.cardType=='social' &&
-        <View style={styles.likecont}>
-        <H6>{likecount} Likes</H6>
-        {like?<AntDesign name="like1" size={29} color={Theme.primary} onPress={()=>{handleLike()}}/> :<AntDesign name="like2" size={29} color="black" onPress={()=>{handleLike()}} />}
-        </View>
-        }
-       
+          <H7 style={styles.prodname}>{props.title}</H7>
+          <View style={styles.reviewCont}>
+            <Rating value={props.overallRating} />
+            <P>({props.overallRating})</P>
+          </View>
+
+          <Pr>
+            {props.price}/{props.unit}
+          </Pr>
+          <H6>By Haritha</H6>
+          {props.cardType == 'social' && (
+            <View style={styles.likecont}>
+              <H6>{likecount} Likes</H6>
+              {like ? (
+                <AntDesign
+                  name='like1'
+                  size={29}
+                  color={Theme.primary}
+                  onPress={() => {
+                    handleLike();
+                  }}
+                />
+              ) : (
+                <AntDesign
+                  name='like2'
+                  size={29}
+                  color='black'
+                  onPress={() => {
+                    handleLike();
+                  }}
+                />
+              )}
+            </View>
+          )}
         </View>
       </View>
-      </TouchableOpacity>
+    </TouchableOpacity>
   );
 }
 const styles = StyleSheet.create({
@@ -65,40 +80,39 @@ const styles = StyleSheet.create({
     backgroundColor: Theme.overlay,
     borderRadius: 20,
     boxShadow: '100px 100px 17px -12px rgba(0,3,0,0.75)',
-    marginVertical:15
+    marginVertical: 15,
   },
-  imgcont:{
-    paddingTop:10,    
-    justifyContent:'center',
-    alignItems:'center'
+  imgcont: {
+    paddingTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   cardimage: {
     width: 135,
     height: 120,
     borderRadius: 10,
-    resizeMode: 'contain',
+    resizeMode: 'cover',
   },
-  prodname:{
-    fontWeight:'bold'
+  prodname: {
+    fontWeight: 'bold',
   },
-  reviewCont:{
-     display:'flex',
-     flexDirection:'row'
+  reviewCont: {
+    display: 'flex',
+    flexDirection: 'row',
   },
-  desccont:{
-    position:'relative',
-    width:155,
+  desccont: {
+    position: 'relative',
+    width: 155,
 
-    paddingHorizontal:10,
-    marginTop:10
+    paddingHorizontal: 10,
+    marginTop: 10,
   },
-  likecont:{
-    display:'flex',
-    alignItems:'center',
-    flexDirection:'row',
-    flexWrap:'wrap',
-    justifyContent:'space-between',
-    paddingHorizontal:2
-  }
-
+  likecont: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    paddingHorizontal: 2,
+  },
 });
