20eac77b81962d0af36cc692a277280e403edfb5
Haritha Hasathcharu <42366642+hasathcharu@users.noreply.github.com>
2023-04-03 09:08:57 +0530

    backend name

--- Freshlyy/constants/env.js
+++ Freshlyy/constants/env.js
@@ -1,4 +1,4 @@
 export default {
-  backend: 'http://192.168.1.8:4000',
-  GOOGLE_API_KEY:"AIzaSyCorJgnsZs2Y8q_c4eMqUtUV_0icAmHWhw"
+  backend: 'http://harithas-macbook-pro.local:4000',
+  GOOGLE_API_KEY: 'AIzaSyCorJgnsZs2Y8q_c4eMqUtUV_0icAmHWhw',
 };
